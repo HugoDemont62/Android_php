58d3ac45af452012cc9818cc405129414b2fd64d
HugoDemont <hugodemont62@hotmail.fr>
2022-10-10 12:46:38 +0200

    commit regi

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 session_start();
 
 try {
-    $pdo = new PDO('mysql:host=host;port=port;dbname=dbname', 'login', 'mdp');
+    $pdo = new PDO('mysql:host=hugodey62.mysql.db;port=port;dbname=hugodey62', 'hugodey62', 'Camille6262');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(Exception $e) {
